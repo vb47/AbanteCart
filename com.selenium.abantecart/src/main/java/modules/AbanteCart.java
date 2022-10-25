5c312d3d9ba484b0e918f47fd56ba92216287a9f
Vaibhav Alur <vsalur22@gmail.com>
2022-10-25 20:22:36 +0530

    Created Class: AbanteCart.java

---
+++ com.selenium.abantecart/src/main/java/modules/AbanteCart.java
@@ -0,0 +1,76 @@
+package modules;
+
+import java.time.Duration;
+import java.util.*;
+import org.openqa.selenium.WebDriver;
+
+/*
+ * Class: AbanteCart
+ * Description: Parent Class to handle basic operations for AbanteCart Website.
+ * Parameters: 	driver - WebDriver object to inherit driver capabilities.
+ * 				url (optional): locally hosted url.
+ */
+public class AbanteCart {
+	WebDriver driver;
+	String url = "https://demo.abantecart.com/";
+	Integer numberOfItemsInCart;
+	String itemsInCart[];
+	Double totalAmount;
+	Integer ImplicitWaitTime = 5;
+	
+	public AbanteCart(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	public AbanteCart(WebDriver driver, String url) {
+		this.driver = driver;
+		this.url = url;
+	}
+	
+	// Essential getters and setters
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public Integer getNumberOfItemsInCart() {
+		return numberOfItemsInCart;
+	}
+	public String[] getItemsInCart() {
+		return itemsInCart;
+	}
+	public Double getTotalAmount() {
+		return totalAmount;
+	}
+	
+	/*
+	 * Method: openWebsite
+	 * Description: Opens website using default url using WebDriver.
+	 */
+	public void openWebsite() {
+		driver.get(url);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(this.ImplicitWaitTime));
+	}
+	
+	/*
+	 * Method: openWebsite
+	 * Parameters: url
+	 * Description: Opens website using default url using WebDriver.
+	 */
+	public void openWebsite(String url) {
+		driver.get(url);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(this.ImplicitWaitTime));
+	}
+	
+	/*
+	 * Method: closeWebsite
+	 * Description: closes the current website handled by WebDriver.
+	 */
+	public void closeWebsite() {
+		driver.close();
+	}
+	
+}
--- com.selenium.abantecart/src/test/java/testModules/mainPageTest.java
+++ com.selenium.abantecart/src/test/java/testModules/mainPageTest.java
@@ -1,25 +1,37 @@
 package testModules;
 
+// Temporary test class, jo bhi test karna he yaha pe karo. 
+
+
 import org.testng.annotations.Test;
 
+import modules.AbanteCart;
 import modules.mainPage;
 
 import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 
 public class mainPageTest {
+	WebDriver driver;
+	AbanteCart cart;
+	
   @Test
-  public void f() throws InterruptedException {
-	  mainPage page = new mainPage();
-	  page.openWebsite();
+  public void fudduTest(){
+	  cart = new AbanteCart(driver);
+	  cart.openWebsite();
 	  
   }
   @BeforeTest
   public void beforeTest() {
+	  driver = new ChromeDriver();
   }
 
   @AfterTest
-  public void afterTest() {
+  public void afterTest() throws InterruptedException {
+	  Thread.sleep(5000);
+	  cart.closeWebsite();
   }
 
 }
